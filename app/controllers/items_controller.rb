9444594f0e1d7ba9e196d43f24780832b091da4b
Tej Patel <tejpatel317@gmail.com>
2022-11-02 18:44:22 -0400

    commit tree aebfe039e570dbd3bd27f4a463f694cf308c5820

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,41 @@
 class ItemsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else 
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items.find(params[:id])
+    else 
+      item = Item.find(params[:id])
+    end
+    render json: item, include: :user
+  end
+
+  def create
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items.create(name: params[:name], description: params[:description], price: params[:price])
+    else 
+      item = Item.create(name: params[:name], description: params[:description], price: params[:price])
+    end
+    render json: item, include: :user, status: :created
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "Bird not found" }, status: :not_found
   end
 
 end
